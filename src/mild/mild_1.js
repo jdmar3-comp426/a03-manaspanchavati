c621b6174156a712db85c593b0e4b9d8155219ae
manaspanchavati <manas@ad.unc.edu>
2021-11-30 03:00:37 -0500

    spacing

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -43,7 +43,7 @@ export function getIncreasingArray(startNumber, endNumber) {
 export function maxAndMin(numbers) {
     let max = Math.max(...numbers);
     let min = Math.min(...numbers);
-    return "max: " + max + ", min: " + min;
+    return " max: " + max + ", min: " + min;
 }
 
 /**
