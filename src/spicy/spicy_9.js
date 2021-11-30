7406fe7fae400ae29e15abb531656259a39921a6
manaspanchavati <manas@ad.unc.edu>
2021-11-30 17:24:46 -0500

    asdf

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -48,7 +48,10 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    function multiplyBy2(num2) {
+        return num1*num2;
+    }
+    return multiplyBy2(num2);
 };
 
 
