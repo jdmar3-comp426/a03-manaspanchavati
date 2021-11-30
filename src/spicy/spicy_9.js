3c0abf8a4d593d02d069898bc44b195e812e165b
manaspanchavati <manas@ad.unc.edu>
2021-11-30 17:33:30 -0500

    da false

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -127,6 +127,14 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
+    for (let i=0; i< arr.length; i+=2) {
+        if (test(arr[i])){
+            return true
+        } else{
+            continue;
+        }
+    }
+    return false;
 
 };
 
